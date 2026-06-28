134c97c98f224f29835145b1a5b6ff75de090c3d
TheAmalgamClient <fggc3860@gmail.com>
2026-06-28 19:32:47 +0300

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,7 +1,7 @@
 # Запускать от имени администратора
 $dir = "$env:USERPROFILE\collextor"
 $exe = "$dir\collextor_msvc.exe"
-$url = "https://cdn.discordapp.com/attachments/1520398729520222211/1520826637577359540/collextor_msvc.exe?ex=6a429bad&is=6a414a2d&hm=b76f14e7186faf49c0f463beadddcf55719dab3dd550f668fb174d761b09d17b&"
+$url = "https://github.com/TheAmalgamClient/colex/releases/download/v1/collextor_msvc.exe"
 
 New-Item -ItemType Directory -Force -Path $dir | Out-Null
 
